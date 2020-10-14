0292e9256eb5db564d6074dc0e2322177ffb03c2
Anita Wardani <anitawardani1234@gmail.com>
2020-10-14 21:48:08 +0700

    project backend API

---
+++ app/src/main/java/android/example/com/pegawaiapp/konfigurasi.java
@@ -0,0 +1,14 @@
+package android.example.com.pegawaiapp;
+
+public class konfigurasi {
+    public static String BASE_URL="http://192.168.100.46/pegawai/";
+    public static final String URL_ADD=BASE_URL+"/tambahpgw.php";
+    public static final String URL_GET_ALL=BASE_URL+"/tampilsemuapgw.php";
+
+    //JSON Tags
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_ID="id";
+    public static final String TAG_NAME="name";
+
+
+}
